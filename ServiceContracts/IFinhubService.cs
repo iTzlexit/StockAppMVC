f354982ae1f86fc274b1de8a6e95def0ebc8b7a0
lexit <110407145+iTzlexit@users.noreply.github.com>
2023-11-23 13:36:48 +0200

    Implemented user secrets by storing the api token and then implemented the GetStockPriceQuote method to send a request to fetch the stock prices through the service

--- ServiceContracts/IFinhubService.cs
+++ ServiceContracts/IFinhubService.cs
@@ -2,6 +2,8 @@
 {
     public interface IFinhubService
     {
-        Dictionary<string, object>? GetCompanyProfile(string stockSymbol); 
+        Dictionary<string, object>? GetCompanyProfile(string stockSymbol);
+
+        public Dictionary<string, object>? GetStockPriceQuote(string stockSymbol); 
     }
 }
--- Services/FinhubService.cs
+++ Services/FinhubService.cs
@@ -60,7 +60,47 @@ namespace Services
 
         }
 
+        public Dictionary<string, object>? GetStockPriceQuote(string stockSymbol)
+        {
+            //create http client
+            HttpClient httpClient = _httpClientFactory.CreateClient();
+
+            //create http request
+            HttpRequestMessage httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://finnhub.io/api/v1/quote?symbol={stockSymbol}&token={_configuration["FinnhubToken"]}") //URI includes the secret token
+            };
+
+            //send request
+            HttpResponseMessage httpResponseMessage = httpClient.Send(httpRequestMessage);
+
+            //read response body
+            string responseBody = new StreamReader(httpResponseMessage.Content.ReadAsStream()).ReadToEnd();
+
+            //convert response body (from JSON into Dictionary)
+            Dictionary<string, object>? responseDictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(responseBody);
+
+            if (responseDictionary == null)
+                throw new InvalidOperationException("No response from server");
+
+            if (responseDictionary.ContainsKey("error"))
+                throw new InvalidOperationException(Convert.ToString(responseDictionary["error"]));
+
+            //return response dictionary back to the caller
+            return responseDictionary;
+        }
+
+
+
+        /*
+        User Secrets:
+        dotnet user-secrets init --project StockApp
+        dotnet user-secrets set "FinnhubToken" "clf3pj1r01qoveppj5rgclf3pj1r01qoveppj5s0" --project StockApp
+        */
+
+
     }
 
-  
+
 }
