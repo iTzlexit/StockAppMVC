2fb9f6b309e3091f8c2154af2ffff8efcafd4aa2
lexit <110407145+iTzlexit@users.noreply.github.com>
2023-11-23 17:04:24 +0200

    implemented the layout and completed the controller

--- StockApp/Controllers/TradeController.cs
+++ StockApp/Controllers/TradeController.cs
@@ -1,21 +1,58 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using ServiceContracts;
+using StockApp.Models;
+using System.Diagnostics;
 
 namespace StockApp.Controllers
 {
     [Route("[controller]")] // /trade will always start with trade 
     public class TradeController : Controller
     {
+        private readonly TradingOptions _tradingOptions;
+        private readonly IFinhubService _finhubService;
+        private readonly IConfiguration _configuration;
 
-     
-        public TradeController()
+        public TradeController(IOptions<TradingOptions> tradingOptions, IFinhubService finhubService, IConfiguration configuration)
         {
-            
+            _tradingOptions = tradingOptions.Value;
+            _finhubService = finhubService;
+            _configuration = configuration;
         }
 
-        public IActionResult Index(IOptions<TradingOptions> tradingOptions, )
+
+        [Route("/")]
+        [Route("[action]")] // route will be trade/index 
+        [Route("~/[controller]")]
+        public IActionResult Index()
         {
-            return View();
+            //reset stock symbol if it isnt present 
+
+            if (string.IsNullOrEmpty(_tradingOptions.DefaultStockSymbol))
+                _tradingOptions.DefaultStockSymbol = "MSFT";
+
+
+            //get company profile from api server 
+
+            Dictionary<string, object>? companyProfileDictionary = _finhubService.GetCompanyProfile(_tradingOptions.DefaultStockSymbol);
+
+            //Get stock price quotes from Api Server 
+
+            Dictionary<string, object>? stockQuoteDictionary = _finhubService.GetStockPriceQuote(_tradingOptions.DefaultStockSymbol);
+
+            //create model object
+            StockTrade stockTrade = new StockTrade() { StockSymbol = _tradingOptions.DefaultStockSymbol };
+
+            //load data from finnHubService into model object
+            if (companyProfileDictionary != null && stockQuoteDictionary != null)
+            {
+                stockTrade = new StockTrade() { StockSymbol = Convert.ToString(companyProfileDictionary["ticker"]), StockName = Convert.ToString(companyProfileDictionary["name"]), Price = Convert.ToDouble(stockQuoteDictionary["c"].ToString()) };
+            }
+
+            //Send Finnhub token to view
+            ViewBag.FinnhubToken = _configuration["FinnhubToken"];
+
+            return View(stockTrade);
         }
     }
 }
--- StockApp/Program.cs
+++ StockApp/Program.cs
@@ -1,8 +1,15 @@
+using ServiceContracts;
+using Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+
+// Services 
 builder.Services.AddControllersWithViews();
 builder.Services.Configure<TradingOptions>(builder.Configuration.GetSection("TradingOptions")); // binds the configuration section named "TradingOptions" from your app settings to a TradingOptions class.
+
+builder.Services.AddSingleton<IFinhubService, FinhubService>();
 
 builder.Services.AddHttpClient();
 
