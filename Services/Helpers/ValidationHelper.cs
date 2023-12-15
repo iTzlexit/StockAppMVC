e18b4ea2829b4d06581295362e01c4977b3aca93
lexit <110407145+iTzlexit@users.noreply.github.com>
2023-12-15 15:40:14 +0200

    Implemented the Buyorder implementation in the Stocks Services

---
+++ Services/Helpers/ValidationHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Helpers
+{
+    public class ValidationHelper
+    {
+        /// <summary>
+        /// Persons model validations using ValidationContext and throws ArgumentException in case of any validation errors
+        /// </summary>
+        /// <param name="obj">Model object to validate</param>
+        /// <exception cref="ArgumentException">When one or more validation errors found</exception>
+
+
+        internal static void ModelValidation(object obj)
+        {
+            //Model Validations
+
+            ValidationContext validationContext = new ValidationContext(obj);
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+
+            //validate the model object and get errors 
+
+            bool isValid = Validator.TryValidateObject(obj, validationContext, validationResults, true);
+
+            if (!isValid)
+            {
+                throw new ArgumentException(validationResults.FirstOrDefault()?.ErrorMessage); 
+            }
+        }
+    }
+}
--- Services/StocksService.cs
+++ Services/StocksService.cs
@@ -1,5 +1,7 @@
-﻿using ServiceContracts;
+﻿using Entities;
+using ServiceContracts;
 using ServiceContracts.DTO;
+using Services.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +12,45 @@ namespace Services
 {
     public class StocksService : IStocksService
     {
+        //Private fields for the in memory data 
+
+        private readonly List<BuyOrder> _buyOrders;
+        private readonly List<SellOrder> _sellOrders;
+
+
+        public StocksService()
+        {
+            _buyOrders = new List<BuyOrder>();
+            _sellOrders = new List<SellOrder>(); 
+
+        }
+
+
+
         public BuyOrderResponse CreateBuyOrder(BuyOrderRequest? buyOrderRequest)
         {
-            throw new NotImplementedException();
+            // Vallidation : buyOrderRequest cant be null 
+
+            if(buyOrderRequest == null) 
+                throw new ArgumentNullException(nameof(buyOrderRequest));
+
+            //Model Validation 
+
+            ValidationHelper.ModelValidation(buyOrderRequest); 
+
+            //convert into by order Type 
+
+            BuyOrder buyOrder = buyOrderRequest.ToBuyOrder();
+
+            // generate a new Buy Order Id 
+
+            buyOrder.BuyOrderID = Guid.NewGuid();
+
+            //Add buy order object to buy ordrs list 
+            _buyOrders.Add(buyOrder);
+            //convert  the BuyOrders object into the buy orders response 
+
+            return buyOrder.ToBuyOrderResponse();
         }
 
         public SellOrderResponse CreateSellOrder(SellOrderRequest? sellOrderRequest)
