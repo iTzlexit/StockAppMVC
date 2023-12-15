1e7ec2505f9d7528578f7bcd43c7e85cebaffbf1
lexit <110407145+iTzlexit@users.noreply.github.com>
2023-12-15 09:45:35 +0200

    Sell Order and Buy Order Entities created

---
+++ Entities/BuyOrder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    /// <summary>
+    /// Represents a buy order to purchase the stocks 
+    /// </summary>
+    public class BuyOrder
+    {
+        [Key] 
+        public Guid BuyOrderID { get; set; }
+        public string StockSymbol { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Stock Name can't be null or empty")]
+        public string StockName { get; set;} = string.Empty;
+        /// <summary>
+        /// Date and time of order, when it is placed by the user
+        /// </summary>
+        public DateTime DateAndTimeOfOrder { get; set; }
+
+        [Range(1, 10000, ErrorMessage = "You can buy maximum of 100000 shares in single order. Minimum is 1.")]
+
+        /// <summary>
+        /// The number of stocks (shares) to buy
+        /// </summary>
+        public uint Quantity { get; set; }
+
+        /// <summary>
+        /// The price of each stock (share)
+        /// </summary>
+        [Range(1, 10000, ErrorMessage = "The maximum price of stock is 10000. Minimum is 1.")]
+        public double Price { get; set; }
+    }
+}
---
+++ Entities/SellOrder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class SellOrder
+    {
+        [Key]
+        public Guid SellOrderId { get; set; }
+        public string StockSymbol{ get; set; } = string.Empty;
+        [Required(ErrorMessage = "Stock name can't be null or empty ")]
+        public string StockName { get; set; } = string.Empty;
+
+        public DateTime DateAndTimeOfOrder { get; set; }
+
+        [Range(1, 100000, ErrorMessage = "You can buy maximum of 100000 shares in single order. Minimum is 1.")]
+        public uint Quantity { get; set; }
+        [Range(1, 10000, ErrorMessage = "The maximum price of stock is 10000. Minimum is 1")]
+        public double Price { get; set; }
+    }
+}
